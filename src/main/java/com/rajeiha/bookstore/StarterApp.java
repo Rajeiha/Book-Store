37c25a8059797dd7ae4e5da50d6b0c250b02fe7e
Rajeiha <rajeiha122333@gmail.com>
2020-10-20 13:33:49 +0530

    second commit

---
+++ src/main/java/com/rajeiha/bookstore/StarterApp.java
@@ -0,0 +1,16 @@
+package com.rajeiha.bookstore;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class StarterApp {
+    public static void main(String[] args){
+        SpringApplication.run(StarterApp.class,args);
+
+    }
+
+
+}
